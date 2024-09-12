9616c38c61c7895b311702f616c779ad784326c3
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-09-11 23:05:39 -0300

    Update ex9.cpp

--- lista01/ex9.cpp
+++ lista01/ex9.cpp
@@ -11,7 +11,13 @@ int main()
     std :: cin >> y1;
     std :: cin >> x2;
     std :: cin >> y2;
+    //outra possibilidade:
+    // std:: cin >> x1 >> y1 >> x2 >> y2;
     
+    //calculando a dist euclidiana usando as funções sqrt(raiz quadrado)
+    // e pow para elevar a potencia de 2 
     dist = sqrt(pow((x1 - x2), 2) + pow((y1 - y2),2));
-    std :: cout  << std:: setprecision(5) << dist ;
+    
+    //usando a função setprecision(x) para definir a precisão de x casas decimais
+    std :: cout  << std:: fixed << std :: setprecision(5) << dist << std::endl;
 }
