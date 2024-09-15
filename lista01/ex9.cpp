95fad2b00058cd564e12fa68ec6018dbf3f70536
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-09-15 11:57:18 -0300

    test 9

--- lista01/ex9.cpp
+++ lista01/ex9.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    double x1, x2, y1, y2, dist;
+    double x1, x2, y1, y2, dist, predist;
 
     std :: cin >> x1;
     std :: cin >> y1;
@@ -19,8 +19,8 @@ int main()
     dist = sqrt(pow((x1 - x2), 2) + pow((y1 - y2),2));
     
     //arredondamento manual
-    dist = std::round(dist *1000.0)/1000.0;
-
-    //usando a função setprecision(x) para definir a precisão de x casas decimais
-    std :: cout  << std:: fixed << std :: setprecision(4) << dist << std::endl;
+    //predist = std::round(dist *1000.0)/1000.0;
+    
+    //usando a função setprecision(x) para definir a precisão de 4 casas decimais
+    std :: cout  << std :: setprecision(5) << dist << std::endl;
 }
