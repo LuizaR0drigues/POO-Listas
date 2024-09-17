5999c3921380337976009fe23d96dcba6dab83dc
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-09-17 10:53:40 -0300

    test2

--- lista01/ex12.cpp
+++ lista01/ex12.cpp
@@ -5,22 +5,28 @@ using namespace std;
 
 int pow(int b, int e, int m)
 {
-   int aux, i=1, resto;
+   int aux, i=0, resto;
    aux = b;
    while (i < e)
    {
+    if (i == 0)
+    {
+        aux *= 1;
+    }
+    else{
      aux *= b;
-     i++;
+    }
+    i++;
      //cout << aux <<  endl;
    }
    
     resto =  aux % m;
-    //cout << resto<< endl;
-   return aux;
+    cout << resto<< endl;
+   return resto;
 }
 
 int main()
 {   int num1, num2, num3;
-    
-    int test = pow(2, 10, 100);
+    cin >> num1 >> num2 >> num3;
+    int test = pow(num1, num2, num3);
 }
