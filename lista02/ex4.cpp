4beb69e7f13d65984db9583d542f71c89a380b82
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-11-02 18:45:35 -0300

    erros do exe4 checados

--- lista02/ex4.cpp
+++ lista02/ex4.cpp
@@ -11,9 +11,9 @@ public:
     Polinomio2(int aux_a =0, int aux_b =0, int aux_c =0){
         if(aux_a ==0)
         {
-            aux_a = 1;
-            aux_b = rand() %100 ;
-            aux_c = rand() % 100;
+           _a = 1;
+            _b = rand() %100 ;
+            _c = rand() % 100;
         }
         else{
             _a = aux_a;
@@ -33,14 +33,14 @@ public:
         {
             
             v[0] = 2.0;
-            v[1] = (-_b - delta) / 2 * _a;
-            v[2] = (-_b + delta) / 2 * _a;
+            v[1] = (-_b - sqrt( delta)) / (2 * _a);
+            v[2] = (-_b +  sqrt( delta)) / (2 * _a);
 
         }
         else if( delta == 0)
         {
             v[0] = 1.0;
-            v[1] = (-_b + delta) / 2 * _a;
+            v[1] = (-_b ) / (2 * _a);
             v[2] = v[1];
 
 
@@ -62,16 +62,10 @@ public:
 
 int main()
 {
-    double *v;
-    Polinomio2 p1(1, 1, 1);
-    v = p1.raizes(); for(int i=0; i<=v[0]; i++) std::cout << v[i] << " " ;
-    std::cout << '\n' << p1(2) << '\n' ;
-    Polinomio2 p2(1, 2, 1);
-    v = p2.raizes(); for(int i=0; i<=v[0]; i++) std::cout << v[i] << " " ;
-    std::cout << '\n' << p2(2) << '\n' ;
-    Polinomio2 p3(1, -3, 2);
-    v = p3.raizes(); for(int i=0; i<=v[0]; i++) std::cout << v[i] << " " ;
-    std::cout << '\n' << p3(2) << '\n' ;
+    Polinomio2 p(1152, 2690, 128);
+    double *v = p.raizes();
+    for(int i=0; i<=v[0]; i++) std::cout << v[i] << " " ;
+    std::cout << p(2) << std::endl;
 
     return 0;
 }
