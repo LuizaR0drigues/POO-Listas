574ec792e10ac02c303c4cac5b9b40e6f4070cc4
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-11-04 23:00:01 -0300

    teste  de main ex2

--- lista02/ex2.cpp
+++ lista02/ex2.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <iostream>
 
+  static long long int calculo_mdc(long long int a, long long int b ){
+        long long int resto = 0;
+        //printf(" MDC a %lld b %lld \n",a, b);
+        if( a <= 0)
+        {
+            return b;
+        }
+        if(b <= 0)
+        {
+            return a;
+        }
+        if(a == b)
+        {
+            //quando ambos forem iguais, o mdc será 
+            return a;
+        }
+        while(b != 0)
+        {
+                resto = a % b;
+                a = b;
+                b = resto;
+        }
+     //printf("Resto2 %lld \n", resto);
+       return a;
+    }
 
 class Fracao
 {
@@ -12,17 +37,21 @@ private:
     void reduz(){
         long long int divisor = calculo_mdc(_numerador, _denominador);
         _numerador /= divisor;
-        _denominador /= divisor;    
+        _denominador /= divisor;  
     };
 
-    
+  
 public:
     Fracao(long long int numerador = 0, long long int denominador = 1){//definindo os valores padroes
         _numerador = numerador;
         _denominador = denominador;
-      //printf("Original ->%lld / %lld \n", _numerador, _denominador);
         reduz();
-      //printf("Redução -> %lld / %lld \n", _numerador, _denominador);
+    };
+   Fracao(double x, double eps ){ //sobrecarga de construtores
+        _numerador = x/eps;
+        _denominador = 1/eps;
+
+        this->reduz();
     };
 
     friend Fracao operator+(const Fracao& a, const Fracao& b)
@@ -62,48 +91,23 @@ public:
         os << f._numerador << '/' << f._denominador;
         return os;
     }
-    long long int calculo_mdc(long long int a, long long int b ){
-        long long int resto = 0;
-        //printf(" MDC a %lld b %lld \n",a, b);
-        if( a <= 0)
-        {
-            return b;
-        }
-        if(b <= 0)
-        {
-            return a;
-        }
-        if(a == b)
-        {
-            //quando ambos forem iguais, o mdc será 
-            return a;
-        }
-        while(b != 0)
-        {
-                resto = a % b;
-                a = b;
-                b = resto;
-        }
-     //printf("Resto2 %lld \n", resto);
-       return a;
-    };
     
 };
 
 int main(){
 Fracao f;
 std::cout << f << " " << double(f) << '\n' ;
-f = f+Fracao(1, 2);
+f = f+Fracao(1LL, 2LL);
 std::cout << f << " " << double(f) << '\n' ;
-f = f+Fracao(1,2);
+f = f+Fracao(1LL,2LL);
 std::cout << f << " " << double(f) << '\n' ;
-f = f*Fracao(3,2);
+f = f*Fracao(3LL,2LL);
 std::cout << f << " " << double(f) << '\n' ;
-f = f/Fracao(3,4);
+f = f/Fracao(3LL,4LL);
 std::cout << f << " " << double(f) << '\n' ;
-f = f-Fracao(1,6);
+f = f-Fracao(1LL,6LL);
 std::cout << f << " " << double(f) << '\n' ;
-f = f*Fracao(0, 99999999999999);
+f = f*Fracao(0LL, 99999999999999LL);
 std::cout << f << " " << double(f) << '\n' ;
 
     return 0;
