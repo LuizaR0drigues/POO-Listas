dbb0380192cdcbac0b73238e5d4d265fb7eb48aa
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-12-06 21:31:35 -0300

    lista 4 - exe1
    Usando pilhas no contexto de STL em c++

---
+++ lista04/ex1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+//declaração da função stack que retonra o kesimo elemto par
+int elemenK(std::stack<int>& pilha, int k){
+    std::stack<int> cpy_pilha;
+    cpy_pilha  = pilha;
+    int cont =0;
+    while( not pilha.empty())// equanto a cpy_pilha tiver elementos
+    {
+        int elem = cpy_pilha.top(); //pega o elem do topo
+        cpy_pilha.pop(); //retir 1 elem da pilha
+
+        //verifica se o topo é par e retorn se qtdpar for igual a k
+        if( elem % 2 == 0) {
+            cont++;
+            if(cont == k )
+            {
+                return elem;
+            }
+        }
+    }
+
+    //caso não, retorna -1
+    return -1;
+}
+
+int main(){
+// INICIALIZAC ̧AO DA PILHA  ̃
+std::stack<int> pilha;
+for(int e=5; e>=1; e--) //inserindo elementos na pilha 
+{
+    pilha.push(e); //push -> insere elementos
+}
+// TESTE DE FATO
+std::cout << elemenK(pilha, 2) << std::endl;
+
+while(not pilha.empty()) {
+    std::cout << pilha.top() << " " ;
+
+    pilha.pop();
+    }
+std::cout << std::endl;
+return 0;
+}
