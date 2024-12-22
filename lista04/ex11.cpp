8939c403974f21860547a1986f72691a48b8b2d4
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-12-21 23:44:08 -0300

    Update ex11.cpp

--- lista04/ex11.cpp
+++ lista04/ex11.cpp
@@ -9,32 +9,30 @@ private:
     int _idade;
 
 public:
-    Pessoa(const std::string& str)
+    Pessoa(const std::string &str)
     {
-        //std::cout <<  str << std::endl;
-        // Regex  nome, idade, cidade, UF e país
+        // std::cout <<  str << std::endl;
+        //  Regex  nome, idade, cidade, UF e país
         std::regex reg(R"(([^,]+) tenho (\d+) anos e moro em (.+)-([A-Z]{2}),([^\"]+))");
         std::smatch match;
 
-        if (std::regex_search(str, match, reg))
-        {
-            /*teste 
-            std::cout << "Nome:" << match[1] << std::endl;
-            std::cout << "Idade:" << match[2] << std::endl;
-            std::cout << "Cidade:" << match[3] << std::endl;
-            std::cout << "UF:" << match[4] << std::endl;
-            std::cout << "País:" << match[5] << std::endl;
-            */
-            // Atribuir os valores capturados
-            _nome = match[1];
-            _idade = std::stoi(match[2].str());  // Converte int
-            _cidade = match[3];
-            _uf = match[4];
-            _pais = match[5];
-        }
+        std::regex_search(str, match, reg);
 
+        /*teste
+        std::cout << "Nome:" << match[1] << std::endl;
+        std::cout << "Idade:" << match[2] << std::endl;
+        std::cout << "Cidade:" << match[3] << std::endl;
+        std::cout << "UF:" << match[4] << std::endl;
+        std::cout << "País:" << match[5] << std::endl;
+        */
+        // transferindo valores
+        _nome = match[1];
+        _idade = std::stoi(match[2].str()); // Converte int
+        _cidade = match[3];
+        _uf = match[4];
+        _pais = match[5];
     }
-    //impressao usando o std::cout<<
+    // impressao usando o std::cout<<
     friend std::ostream &operator<<(std::ostream &os, const Pessoa &A)
     {
         os << A._nome << std::endl;
